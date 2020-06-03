8dbc852c9cee55166bd54133ad0125373fdf657e
soya <soyalee91@gmail.com>
2020-06-04 00:35:24 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MyName from './MyName';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyName name ="sy"/>
+      </div>
+    );
+  }
+}
+
+export default App;
